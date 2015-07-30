72e5792be5f4023da7f640e69b153a0e8d38a9ae
Gustavo Amigo <g.amigo@99taxis.com>
2015-07-30 14:09:24 -0300

    Fixes

--- src/main/scala/FutureFailureTraining.scala
+++ src/main/scala/FutureFailureTraining.scala
@@ -1,14 +1,16 @@
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Success, Failure}
 
 object FutureFailureTraining extends App{
+
   def test1 = {
-    val future = Future[String] { Thread.sleep(10) ; throw new Exception("No future") }
+    val future = Future[String] { Thread.sleep(10) ; throw new Exception("No future at all") }
     future.map(str => println(str))
   }
 
   def test2 = {
-    val future = Future[String] { Thread.sleep(10) ; throw new Exception("No future") }
+    val future = Future[String] { Thread.sleep(10) ; throw new Exception("No future at all") }
     future
       .map {
         str => println(str) }
@@ -22,18 +24,30 @@ object FutureFailureTraining extends App{
     future.recover {
         case e:IllegalArgumentException => e.getMessage
     }.map {
-      str => println(str) }
+      str => println(str)
+    }
   }
 
   def test4 = {
-    val future1 = Future[String] { Thread.sleep(10) ; throw new IllegalArgumentException("Recovered from this") }
-    val future2 = Future { "Fall back to safe place" }
-    future1
-      .fallbackTo(future2)
+    val theFuture1 = Future[String] { Thread.sleep(10) ; throw new IllegalArgumentException("Recovered from this") }
+    val theFuture2 = Future { "Fall back to safe place" }
+    theFuture1
+      .fallbackTo(theFuture2)
       .map(println)
   }
 
-  test4
+  def test5 = {
+    def log(str: String) = println (str)
+    val future = Future[String] { Thread.sleep(10) ; /*throw new Exception("No future at all")*/ "Done" }
+
+    future
+      .andThen {
+        case Success(s) => log (s"log::Success $s")
+        case Failure(e) => log (s"log::Failure $e")
+      }.map(println)
+  }
+
+  test1
 
   scala.io.StdIn.readLine()
 }
